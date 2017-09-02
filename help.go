f053c2f194d60aa9066aef24b79d1e93d29ef481
Omar H <contact@omarh.net>
2017-09-02 19:41:18 +0200

    Create internal module initiliazation, and add desc to type Module.

--- help.go
+++ help.go
@@ -5,16 +5,26 @@ import (
 )
 
 func init() {
+	moduleName := "Help"
+	commands := map[string]*Command{}
+
 	helpCommand := &Command{
 		Command: ".help",
 		Alias:   ".h",
 		Desc:    "Help shows you a help menu for a given module, or a list of modules.",
 		Usage:   ".help [module-name]",
-		Init:    helpInit,
 		Handler: helpHandler,
 	}
 
-	reservedCommands[helpCommand.Command] = helpCommand
+	commands[helpCommand.Command] = helpCommand
+
+	helpModule := &internalModule{
+		Name:     moduleName,
+		Commands: commands,
+		Init:     helpInit,
+	}
+
+	internalModules[helpModule.Name] = helpModule
 }
 
 func helpInit(s *Snorlax) {
