ce937fcfdc4ec853b3fe51111ee9376930c2af30
Omar H <contact@omarh.net>
2017-09-06 19:59:15 +0200

    Change GetStringFromQuotes to better ver; make events use goroutine.

--- utils/utils.go
+++ utils/utils.go
@@ -11,48 +11,34 @@ func ExtractUserIDFromMention(mention string) string {
 
 // GetStringFromQuotes finds a "string which spans multiple spaces" in a split message.
 // Then takes that and replaces the Quote string with a single string value of the quote contents.
-func GetStringFromQuotes(parts []string) []string {
+func GetStringFromQuotes(parts []string) {
 	var (
-		// found is for controlling the loop-
-		found bool
 		// str is the string we're searching for in quotes.
 		str string
 		// startQuote holds the location of the quote
 		startQuote int
 	)
 
-	for k, val := range parts {
-		if !found {
-			if str == "" && val[0] == '"' {
-				// startQuote location is equal the index of the current value.
-				startQuote = k
-				if val[len(val)-1] == '"' {
-					// The string has been found, mark this so it stops looping.
-					found = true
-					// Since the quoted message is in one location in the slice, we'll just remove the quotes and replace it.
-					parts[k] = val[:len(val)-1][1:]
-				} else {
-					str = str + val[1:] + " "
-				}
-				// If string isn't started just skip.
-			} else if str != "" {
-				// If last char of val is a quote, finish the string search.
-				if val[len(val)-1] == '"' {
-					found = true
-					// Explanation: append the result of append(parts[:startQuote], str+" "+val[:len(val)-1]) with parts[k+1:]...
-					// First in append(parts[:startQuote], str+" "+val[:len(val)-1]) we're appending parts[:startQuote]
-					// parts[:startQuote] is contents of parts which came before where the first quote started.
-					// Then we're appending the quoted message, which we've extracted.
-					// We're taking this new slice (parts[:startQuote] + the quoted message) and appending parts[k+1:]...
-					// parts[k+1:]... is the contents of parts which came before the last quote.
-					// The last quote location is in the current index (stored in var k).
-					parts = append(append(parts[:startQuote], str+val[:len(val)-1]), parts[k+1:]...)
-				} else {
-					str = str + val + " "
-				}
+	length := len(parts)
+	val := ""
+	startQuote = -1
+	for k := 0; k < length; k++ {
+		val = parts[k]
+		switch {
+		case val[0] == '"' && val[len(val)-1] == '"':
+			parts[k] = val[:len(val)-1][1:]
+		case val[0] == '"' && startQuote == -1:
+			startQuote = k
+			str = val[1:] + " "
+		case val[len(val)-1] == '"' && startQuote >= 0:
+			parts = append(append(parts[:startQuote], str+val[:len(val)-1]), parts[k+1:]...)
+			k -= (length - len(parts)) + 1
+			length = len(parts)
+			startQuote = -1
+		default:
+			if startQuote >= 0 {
+				str = str + val + " "
 			}
 		}
 	}
-
-	return parts
 }
