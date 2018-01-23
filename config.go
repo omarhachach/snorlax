0eef20d37da20aeb61436600011f3d050109bf80
Omar A. Hachach <contact@omarh.net>
2018-01-24 00:32:47 +0100

    Create config file update function.

--- config.go
+++ config.go
@@ -3,7 +3,10 @@ package snorlax
 import (
 	"encoding/json"
 	"io/ioutil"
+	"os"
 )
+
+var cachedConfigPath string
 
 // Config is the configuration struct for the Snorlax bot.
 type Config struct {
@@ -17,8 +20,25 @@ type Config struct {
 	Owners        []string `json:"owners"`
 }
 
+// UpdateFile will update the config.json file.
+func (c *Config) UpdateFile() error {
+	newFile, err := json.MarshalIndent(*c, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	file, err := os.OpenFile(cachedConfigPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+
+	file.Write(newFile)
+	return nil
+}
+
 // ParseConfig parses a config file, and returns a new Config.
 func ParseConfig(path string) (*Config, error) {
+	cachedConfigPath = path
 	bytes, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
