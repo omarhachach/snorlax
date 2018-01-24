fff547c754e3c64199e4e2a5cc6c8c83cc3ffe97
Omar A. Hachach <contact@omarh.net>
2018-01-24 21:18:26 +0100

    Create cache clear command.

---
+++ modules/administration/session.go
@@ -0,0 +1,27 @@
+package administration
+
+import (
+	"github.com/bwmarrin/discordgo"
+	"github.com/omar-h/snorlax"
+)
+
+func init() {
+	clearStateCommand := &snorlax.Command{
+		Command:    ".clearstate",
+		Alias:      ".clearcache",
+		Usage:      ".clearstate",
+		ModuleName: moduleName,
+		Handler:    clearStateHandler,
+	}
+
+	commands[clearStateCommand.Command] = clearStateCommand
+}
+
+func clearStateHandler(ctx *snorlax.Context) {
+	if !ctx.Snorlax.IsOwner(ctx.Message.Author.ID) {
+		ctx.SendErrorMessage("You have to be a bot owner to run this command.")
+		return
+	}
+
+	ctx.State = discordgo.NewState()
+}
